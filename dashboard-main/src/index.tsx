d3102a9a1b2c3a59b4abda34e8606a0d10799081
betowiatrowski <beto.wiatrowski@gmail.com>
2022-05-13 12:59:20 -0300

    Add files via upload

---
+++ dashboard-main/src/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
+import App from './App';
+import './index.css'
+import store from './store/index';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <HashRouter >
+        <App />
+      </HashRouter>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
